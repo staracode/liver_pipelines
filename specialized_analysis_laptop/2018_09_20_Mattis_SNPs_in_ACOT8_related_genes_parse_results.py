85696b8850129836352148006908ca3dd2e2c399
Tara <tara.friedrich@ucsf.edu>
2018-09-24 21:08:28 -0700

    parse the last section of the VCF file so that Aras could have more readable results

--- specialized_analysis_laptop/2018_09_20_Mattis_SNPs_in_ACOT8_related_genes_parse_results.py
+++ specialized_analysis_laptop/2018_09_20_Mattis_SNPs_in_ACOT8_related_genes_parse_results.py
@@ -3,8 +3,10 @@ import sys
 #parse results of intersection of VCF with regions of interest 
 snps = "/Users/tfriedrich/Downloads/Mattis_genes_snps.txt"
 outputfile = "/Users/tfriedrich/Downloads/Mattis_genes_snps_parsed.txt"
+columns = ["chrom", "snp_position", "snp_id", "reference_allele", "alternative_allele", "gene_of_interest", "non-synonymous_stop_codon_present", "non-synonymous_missense_present", "non-sysnonymous_frameshit_present", "freqeuncies_major_minor_1000Genomes", "frequencies_major_minor_TOPMED"]
 snps_filehandle = open(snps, "r")
 outhandle = open(outputfile, "w")
+outhandle.write("\t".join(columns) + "\n")
 for line in snps_filehandle: 
     if "ACOT8" in line:  # initially Aras Mattis was mainly interested in ACOT8 
         fields = line.rstrip("\n").split()
@@ -13,8 +15,33 @@ for line in snps_filehandle:
         snp_name = fields[11]
         ref = fields[12]
         alt = fields[13]
-        anno = fields[16].split(";")  # I am having trouble parsing this
-        outhandle.write("\t".join ([chrom, snp_start, snp_name,ref, alt, "\t".join(anno)]))
+        [ freq1 , freq2 ] = [ "NA", "NA"]
+        anno = fields[16].split(";")  # parse this line 
+        for info in anno: 
+            if "TOPMED" in info: 
+                freq1 = info.replace("TOPMED=", "").split(",")
+                
+            if "CAF" in info: 
+                freq2 = info.replace("CAF=", "").split(",")
+
+            if "GENEINFO" in info: 
+                gene = info.replace("GENEINFO=", "")
+
+        [NSN, NSM, NSF] = [0,0,0]
+        if "NSN" in anno:
+                NSN=1
+            
+        if "NSM" in anno:
+                NSM=1
+             
+        if "NSF" in anno:
+                NSF=1
+         
+            
+
+        # column description 
+        
+        outhandle.write("\t".join ([chrom, snp_start, snp_name,ref, alt, "\t".join(map(str, [gene, NSN, NSM, NSF, freq1, freq2]))]))
         outhandle.write("\n")
 outhandle.close()
 snps_filehandle.close()
