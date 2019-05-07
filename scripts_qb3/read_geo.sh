2801290ead8927e51e9772593bfa975235566b4a
Tara <tara.friedrich@ucsf.edu>
2019-05-06 18:58:09 -0700

    script to extra fastq from geo; doesn't work on wynton because the nodes it runs on don't have access to the internet

--- scripts_qb3/read_geo.sh
+++ scripts_qb3/read_geo.sh
@@ -1,9 +1,26 @@
-files=`cut -d , -f 9 2019_05_02_Willenbring_rnaseq_tf_cholangiocyte_hepatocyte/GSE108315_SRA.csv`
+#!/bin/bash                         #-- what is the language of this shell
+#                                  #-- Any line that starts with #$ is an instruction to SGE
+#$ -S /bin/bash                     #-- the shell for the job
+#$ -o test_stdout                       #-- output directory (fill in)
+#$ -e test_stderr                        #-- error directory (fill in)
+#$ -cwd                            #-- tell the job that it should start in your working directory
+#$ -r y                            #-- tell the system that if a job crashes, it should be restarted
+##$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
+#$ -l mem_free=10G                  #-- submits on nodes with enough free memory (required)
+#$ -l scratch=10G         #-- SGE resources (home and scratch disks)
+#$ -l h_rt=24:00:00                #-- runtime limit (see above; this requests 24 hours)
 
-echo $files
+# Directory where fastq file for project reside
+DIR_NAME=$1
+
+# File listing all the fastq file names and identifiers
+FILE1=$2
+#FILE1=2019_05_02_Willenbring_rnaseq_tf_cholangiocyte_hepatocyte/GSE108315_HW.csv
+
+files=`cut -d , -f 9 ${FILE1}` 
 
 for fastq in $files; do 
 	if [[ $fastq == *"SR"* ]]; then
-		echo "LiverCenter/software_source/sratoolkit.2.9.6-centos_linux64/bin/fastq-dump " $fastq
-	fi 
+		echo "LiverCenter/software_source/sratoolkit.2.9.6-centos_linux64/bin/fastq-dump " $fastq " --outdir ${DIR_NAME}/fastq/ --outdir fastq --gzip --skip-technical  --readids --read-filter pass --dumpbase --split-3 --clip SRR_ID "  
+	fi
 done
